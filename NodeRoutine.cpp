b6855321a16cca3559caf3e6e6313e9c2a17de66
Luke Hofstetter <ldhofstetter@gmail.com>
2024-05-17 17:48:43 -0700

    [spec] full LML protocol spec

--- NodeRoutine.cpp
+++ NodeRoutine.cpp
@@ -3,6 +3,8 @@
 
 using namespace std;
 
+const int placeholder_noise = -75;
+
 double epoch_double(struct timespec *tv) {
   if (clock_gettime(CLOCK_REALTIME, tv)) {
     cout << "ERROR: clock_gettime function call failed." << endl;
@@ -91,17 +93,38 @@ int main() {
     /*
         LML Protocol:
         {
-            type: pairing | signal_data | candidate,
+            type: pairing | signal_data | candidate | configure,
+            noise: noise_level_in_dB, (this will hopefully, at least in the future, allow us to assign a weight to each distance calc on how much we can rely on it, this
+            also allows us to change protocol if the channel is noisy)
             data: {
                 IF type = signal_data
-                devices: []
-                
+                devices: {
+                    name_of_device: distance_from_device
+                }
+                ELSE IF type = candidate
+                device: device_name,
+                action: add | remove,
+                ELSE IF type = configure
+                action: disconnect | change_port | change_protocol,
+                port: number_of_port (if action = change_port or change_protocol),
+                protocol: 0 | 1 (0 = UDP, 1 = TCP. Port num must also be included. Only used if action = change_protocol)
+                ELSE
+                socket_to_communicate: socket_number, (only sent if from parent to child)
+                type_of_socket_used_for_communication: 0 | 1, (0 = UDP, 1 = TCP, allows for flexible transport layer configuration)
+                interval: x ms, (interval rate that the node should send data to parent, only sent from parent to child)
             }
         }
     
     */
     
-    string msg = "{\n type:pairing,\n }";
+    string msg = "{\n type:\"pairing\",\n noise:" + to_string(placeholder_noise) + "\n}";
+
+    sockaddr_in6 broadcast;
+    in6_addr broadcast_addr;
+
+    unsigned char buf[sizeof(struct in6_addr)];
+    
+    inet_pton(AF_INET6, "ff02::1", buf);
 
     while ((epoch_double(&alttv) - connection_wait_begin) < DEFAULT_WAIT) { // waiting for other nodes to pair
         if (recvfrom(sockfd, node_message, sizeof(node_message), 0, (struct sockaddr *)&client_address, &client_struct_size) > 0) {
