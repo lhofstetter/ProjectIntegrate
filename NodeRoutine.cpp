bc4db22a774880b671c082a2d4eca093d1a0802e
Marley Willyoung <108915937+marleyyvon@users.noreply.github.com>
2024-05-18 15:32:49 -0700

    lml

--- NodeRoutine.cpp
+++ NodeRoutine.cpp
@@ -177,33 +177,6 @@ int main()
 
     logmsg(begin, &alttv, &logfile, "Setup successful. Listening for other nodes...", true);
 
-    /*
-        LML Protocol:
-        {
-            type: pairing | signal_data | candidate | configure,
-            noise: noise_level_in_dB, (this will hopefully, at least in the future, allow us to assign a weight to each distance calc on how much we can rely on it, this
-            also allows us to change protocol if the channel is noisy)
-            data: {
-                IF type = signal_data
-                devices: {
-                    name_of_device: distance_from_device
-                }
-                ELSE IF type = candidate
-                device: device_name,
-                action: add | remove,
-                ELSE IF type = configure
-                action: disconnect | change_port | change_protocol,
-                port: number_of_port (if action = change_port or change_protocol),
-                protocol: 0 | 1 (0 = UDP, 1 = TCP. Port num must also be included. Only used if action = change_protocol)
-                ELSE
-                socket_to_communicate: socket_number, (only sent if from parent to child)
-                type_of_socket_used_for_communication: 0 | 1, (0 = UDP, 1 = TCP, allows for flexible transport layer configuration)
-                interval: x ms, (interval rate that the node should send data to parent, only sent from parent to child)
-            }
-        }
-
-    */
-
     string msg = "{\n type:\"pairing\",\n noise:" + to_string(placeholder_noise) + "\n}";
 
     sockaddr_in6 broadcast;
--- RSSI.cpp
+++ RSSI.cpp
@@ -3,15 +3,16 @@
 #include <pthread.h>
 
 void getDeviceID(pcap_if_t **all_devs, pcap_if_t **node_curr, char error_buff[], char **devID, bool debug);
-//void packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, const u_char* packet_data);
+// void packet_handler(u_char* args, const struct pcap_pkthdr* packet_header, const u_char* packet_data);
 void *sniffer();
 
-int main() {
+int main()
+{
 
     /* Parameters for device recognition */
     pcap_if_t *alldevs; /* List that holds all network devices */
-    pcap_if_t *node; /* Node used for sniffing */
-    char *dev_ID; /* Name of device */
+    pcap_if_t *node;    /* Node used for sniffing */
+    char *dev_ID;       /* Name of device */
 
     /* Parameters for packet capture */
     pcap_t *dev_handler; /* Handler for reading pkt data */
@@ -22,105 +23,109 @@ int main() {
 
     char error_buffer[PCAP_ERRBUF_SIZE]; /* Error buffer */
 
-    getDeviceID(&alldevs,&node,error_buffer,&dev_ID,false);
+    getDeviceID(&alldevs, &node, error_buffer, &dev_ID, true);
 
-    if( (dev_handler = pcap_create(dev_ID,error_buffer)) == NULL){
-        printf("Error creating handler %s\n",error_buffer);
+    if ((dev_handler = pcap_create(dev_ID, error_buffer)) == NULL)
+    {
+        printf("Error creating handler %s\n", error_buffer);
         exit(1);
     }
 
     int err = pcap_activate(dev_handler);
 
-    if( err == 0){
+    if (err == 0)
+    {
         printf("Device handler activated sucessfully!\n");
-    }else if( err > 0 ){
+    }
+    else if (err > 0)
+    {
         printf("Device handler activated with warnings!\n");
-    }else{
+    }
+    else
+    {
         printf("Device handler activation failed!\n");
         exit(1);
     }
 
+    //     //Need threads one for listening, other for other tasks
 
-//     //Need threads one for listening, other for other tasks
+    //     pthread_t sniff;
 
-//     pthread_t sniff;
+    //     pthread_create(&sniff,NULL,sniffer,/*args*/);
 
-//     pthread_create(&sniff,NULL,sniffer,/*args*/);
+    //     /*
+    //     Use pcap_loop to constantly listen for packets
+    //     Once a packet is received, search for the OSI,
+    //     if the OSI is APPLE or SAMSUNG or GOOGLE
+    //         then grab the MAC address and IP address of this packet and store
+    //         these value somewhere.
 
-//     /*
-//     Use pcap_loop to constantly listen for packets
-//     Once a packet is received, search for the OSI, 
-//     if the OSI is APPLE or SAMSUNG or GOOGLE
-//         then grab the MAC address and IP address of this packet and store 
-//         these value somewhere.
-    
+    //     Using the IP addresses from the list.  Listen to packets
+    //     if you found a packet with that MAC address
+    //         get it, seearch for the signal strength value and
+    //         send this value back to the parent node
 
-//     Using the IP addresses from the list.  Listen to packets
-//     if you found a packet with that MAC address
-//         get it, seearch for the signal strength value and
-//         send this value back to the parent node
+    //     Do not need to implement socket logic.
 
-//     Do not need to implement socket logic.
-    
+    //     dev_handler = pcap_open_live(
+    //         dev_ID,
+    //         BUFSIZ,
+    //         packet_count_limit,
+    //         timeout_limit,
+    //         error_buffer
+    //     );
 
+    //     packet = pcap_next(dev_handler,&packet_header);
 
-//     dev_handler = pcap_open_live(
-//         dev_ID,
-//         BUFSIZ,
-//         packet_count_limit,
-//         timeout_limit,
-//         error_buffer
-//     );
+    //     if(!packet){
+    //         return 1;
+    //     }
 
+    //     char errbuf[PCAP_ERRBUF_SIZE];
+    //     pcap_t* handle;
 
-//     packet = pcap_next(dev_handler,&packet_header);
+    //     // Open the network interface for packet capturing
+    //     handle = pcap_open_live("your_network_interface", BUFSIZ, 1, 1000, errbuf);
+    //     if (handle == NULL) {
+    //         std::cerr << "Couldn't open device: " << errbuf << std::endl;
+    //         return 1;
+    //     }
 
-//     if(!packet){
-//         return 1;
-//     }
+    //     // Set a filter to capture only Wi-Fi packets if needed
+    //     struct bpf_program fp;
+    //     pcap_compile(handle, &fp, "wlan", 0, PCAP_NETMASK_UNKNOWN);
+    //     pcap_setfilter(handle, &fp);
 
+    //     // Start capturing packets and call packet_handler for each packet
+    //     pcap_loop(handle, 0, packet_handler, NULL);
 
-
-
-//     char errbuf[PCAP_ERRBUF_SIZE];
-//     pcap_t* handle;
-
-//     // Open the network interface for packet capturing
-//     handle = pcap_open_live("your_network_interface", BUFSIZ, 1, 1000, errbuf);
-//     if (handle == NULL) {
-//         std::cerr << "Couldn't open device: " << errbuf << std::endl;
-//         return 1;
-//     }
-
-//     // Set a filter to capture only Wi-Fi packets if needed
-//     struct bpf_program fp;
-//     pcap_compile(handle, &fp, "wlan", 0, PCAP_NETMASK_UNKNOWN);
-//     pcap_setfilter(handle, &fp);
-
-//     // Start capturing packets and call packet_handler for each packet
-//     pcap_loop(handle, 0, packet_handler, NULL);
-
-//     // Close the packet capture handle
-//     pcap_close(handle);
-// */
+    //     // Close the packet capture handle
+    //     pcap_close(handle);
+    // */
     return 0;
 }
 
-void getDeviceID(pcap_if_t **all_devs, pcap_if_t **node_curr, char error_buff[], char **devID, bool debug){
-    if( pcap_findalldevs(all_devs, error_buff) == 0 ){
+void getDeviceID(pcap_if_t **all_devs, pcap_if_t **node_curr, char error_buff[], char **devID, bool debug)
+{
+    if (pcap_findalldevs(all_devs, error_buff) == 0)
+    {
         printf("Network Devices Found\n");
         *node_curr = *all_devs;
-        while ((*node_curr) -> next != NULL) {
-            if(debug==true)
+        while ((*node_curr)->next != NULL)
+        {
+            if (debug == true)
                 printf("Name of device is %s \n", (*node_curr)->name);
-            if( ((*node_curr) -> flags & PCAP_IF_WIRELESS) && ((*node_curr) -> flags & PCAP_IF_CONNECTION_STATUS_DISCONNECTED)){
+            if (((*node_curr)->flags & PCAP_IF_WIRELESS) && ((*node_curr)->flags & PCAP_IF_CONNECTION_STATUS_DISCONNECTED))
+            {
                 *devID = (*node_curr)->name;
                 break;
             }
-            *node_curr = (*node_curr) -> next;
+            *node_curr = (*node_curr)->next;
         }
-    }else{  /* Device not found */
-        printf("Error finding device %s\n",error_buff);
+    }
+    else
+    { /* Device not found */
+        printf("Error finding device %s\n", error_buff);
         exit(1);
     }
 }
