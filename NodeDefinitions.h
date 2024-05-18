9de3828172bf58f6612621c15a3af1f95efca4c9
Marley Willyoung <108915937+marleyyvon@users.noreply.github.com>
2024-05-18 12:35:53 -0700

    h file for TCP/UDP

--- NodeDefinitions.h
+++ NodeDefinitions.h
@@ -1,3 +1,6 @@
+#ifndef NODE_DEFINITIONS_H
+#define NODE_DEFINITIONS_H
+
 #include <cstdio>
 #include <cstdlib>
 #include <cstring>
@@ -14,6 +17,10 @@
 #include <fcntl.h>
 #include <pcap/pcap.h>
 #include <unistd.h>
+#include <array>
+#include <memory>
+#include <string>
+#include <regex>
 
 #define PAIRING_PORT 8082
 #define DEFAULT_WAIT 10.0
@@ -22,8 +29,9 @@
 #define WARN 1
 #define ERROR 2
 
-/*
-struct node {
-    
-};
-*/
+void send_udp_packet(const std::string &message, const std::string &ip, int port);
+void send_tcp_packet(const std::string &message, const std::string &ip, int port);
+std::string exec(const char *cmd);
+int get_noise_level(const std::string &interface);
+
+#endif // End Macro guard (we can take off if there are issues)
